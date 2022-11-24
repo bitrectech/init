fdf8e9a8dafb5089cf9aeaf8edf25997d13446ec
brcmask7 <2621886267@qq.com>
2022-11-24 23:03:19 +0800

    ✨ feat:初始化脚本更新主机提示符

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#初始化PS1
+echo 'wheit=$(tput setaf 7);yellow=$(tput setaf 3);green=$(tput setaf 10);cyan=$(tput setaf 4);reset=$(tput sgr0);PS1="\[$wheit\]\u\[$yellow\].\[$green\]\h \[$cyan\]\W \[$wheit\]\[$reset\]"' >> /root/.bashrc
+source /root/.bashrc
+
